84f7fd53757966d4e7c7c27cd3adbb41fba98f9a
auraya33 <kasia.nagel@gmail.com>
2018-01-15 17:11:22 +0100

    add animation swing to team photos

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,5 +1,7 @@
 const $sectionFunctions = $("#functions_2");
 const $sectionFuncOffset = $sectionFunctions.offset().top;
+const $AboutTeamPhotos = $(".photo");
+const $aboutTeamOffset = $AboutTeamPhotos.offset().top;
 
 function animateSecondFunctions() {
     let windowBottomEdge = $(window).scrollTop() + $(window).height();
@@ -8,5 +10,15 @@ function animateSecondFunctions() {
         $sectionFunctions.addClass('fadeInUp');
     }
 }
+
 $(window).on('scroll', animateSecondFunctions);
 
+function animateTeamPhotos() {
+    let windowBottomEdge = $(window).scrollTop() + $(window).height();
+
+    if (windowBottomEdge > $aboutTeamOffset) {
+        $AboutTeamPhotos.addClass('swing');
+    }
+}
+
+$(window).on('scroll', animateTeamPhotos);
