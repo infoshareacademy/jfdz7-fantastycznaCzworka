d2e3e6c2e9138a307691acb97bc97bbc7c2bb025
Ewa <32235927+EwaWWW@users.noreply.github.com>
2018-01-24 19:39:15 +0100

    Update antybot.js

--- assets/js/antybot.js
+++ assets/js/antybot.js
@@ -20,27 +20,3 @@ $(function () {
 
 
 
-
-
-
-
-
-
-
-// const a = Math.round(Math.random()*10);
-// const b = Math.round(Math.random()*10);
-// $('.code_antybot').text(`${a} + ${b}`);
-// const sum = a + b;
-//
-// function isInputInvalid() {
-//     let x = $('.input_antybot').val();
-//     return sum !== parseInt(x);
-// }
-//
-// function submitDisability() {
-//     $('.antybot_btn').prop('disabled', isInputInvalid)
-// }
-//
-// $('.input_antybot').on('keyup', function(){
-//     submitDisability();
-// });
