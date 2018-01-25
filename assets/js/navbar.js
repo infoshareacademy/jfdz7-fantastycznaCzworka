d16c3bc0ffae2f42809db5dd3ff8b92a9ac86355
auraya33 <kasia.nagel@gmail.com>
2018-01-25 21:35:17 +0100

    script menu

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -27,3 +27,39 @@ $(document).ready(function(){
         }
     });
 });
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(document).scrollTop() > 100) {
+            $('nav').addClass('shrink');
+
+        }
+        else {
+            $('nav').removeClass('shrink');
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(document).scrollTop() > 700) {
+            $('nav').addClass('opacity');
+
+        }
+        else {
+            $('nav').removeClass('opacity');
+        }
+    });
+});
+
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
