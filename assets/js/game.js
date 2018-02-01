c6a3b010a38af6513263ddf1b1809f1ca9d44141
auraya33 <kasia.nagel@gmail.com>
2018-02-01 17:18:28 +0100

    correct styling for time and score section

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,10 +2,10 @@
 $(window).ready(function() {
 
     let timer = 5;
-    $('.timer').html(timer);
+    $('#timer').html(timer);
     setInterval(function() {
         if(timer > 0) {
-            $('.timer').html(--timer);
+            $('#timer').html(--timer);
         }
         else {
             console.log('koniec')
