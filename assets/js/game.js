bc0f5196f94bd6a7c967c28f9d167b55a3b36e0f
auraya33 <kasia.nagel@gmail.com>
2018-02-04 17:00:07 +0100

    add popup for win/loose action

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,20 @@ $(window).ready(function() {
             $('#timer').html(--timer);
         }
         else {
+            // wywołanie po zakonczeniu czasu
             console.log('koniec')
 
         }
     }, 1000);
 
 });
+
+
+//Popup script
+
+const openPopup = () => {
+    $('#endgame').toggleClass('popup-open')
+};
+
+$('#openbutton').on('click', openPopup);
+
