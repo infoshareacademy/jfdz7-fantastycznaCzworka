bdd25cb80d9f8749541e66f5fc65eca9148aa790
Pablo-Es <szyposz@gmail.com>
2018-02-19 22:48:01 +0100

    added scroll to section function

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
+
+    $(window).on('scroll', function() {
         if($(document).scrollTop() > 100) {
             $('nav').addClass('shrink');
 
@@ -7,14 +7,46 @@ $(document).ready(function() {
         else {
             $('nav').removeClass('shrink');
         }
+
+
+
     });
-});
+
+const $page = $('body, html');
+function changePageTitle() {
+    const anchor = $(this).attr('href');
+    const $activeEl = $('.active');
 
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+}
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
+function scrollToSection() {
+    const animationTime = 500;
+    const anchor = $(this).attr('href');
+
+    $(this)
+        .parent()
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+
+    $page.animate({ scrollTop: $(anchor).offset().top }, animationTime);
+}
+function getCoordinateSection() {
+    const $sections = $('.anchor');
+    $sections.each(function () {
+        console.log($(document).scrollTop());
+        console.log($(this).offset().top);
+         if($(document).scrollTop() ===  $(this).offset().top) {
+
+            document.title = $(this).text();
+         }
+         else console.log('blad');
+    });
+}
+
+
+let $anchorLinks = $('.menu-nav__list').find('a[href^="#"]');
+$(window).on('scroll', getCoordinateSection);
+$anchorLinks.on('click',scrollToSection);
+
